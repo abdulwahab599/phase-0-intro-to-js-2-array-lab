263c8946142a7e50ecd4b5581530bc9ba039452c
Abdul Wahab Khan <abdulwahabchughtai99@gmail.com>
2023-11-26 01:42:59 -0500

    all tests passed

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield"]
+
+
+function destructivelyAppendCat(name){
+    cats.splice(3,0,name)
+}
+
+function destructivelyPrependCat(name){
+    cats.splice(0,0,name)
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.splice(0,1)
+}
+
+function appendCat(name){
+    let newCats = [...cats,name]
+    return newCats
+}
+
+function prependCat(name){
+    let newCats = [name,...cats]
+    return newCats
+}
+
+function removeLastCat(){
+    let newCats = [...cats]
+    newCats.pop()
+    return newCats
+}
+function removeFirstCat(){
+    let newCats = [...cats]
+    newCats.shift()
+    return newCats
+}
+
+
+destructivelyAppendCat('Ralph')
+destructivelyPrependCat('Ralph')
+destructivelyRemoveLastCat()
+destructivelyRemoveFirstCat()
+appendCat('Arnold')
+prependCat('Arnold')
+removeLastCat()
+removeFirstCat()
